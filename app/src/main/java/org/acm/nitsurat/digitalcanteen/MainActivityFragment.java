7d30b2cda0af6d58dd3ff2e23c16a36fb20a3837
KaANO-8 <kaanorahul8@gmail.com>
2016-03-12 23:54:03 +0530

    Integrated code for the first 4 tasks
    Revert "Added nav drawer"
    
    Added nav drawer
    
    Added icons to nav bar
    
    changes made in idea.xml

--- app/src/main/java/org/acm/nitsurat/digitalcanteen/MainActivityFragment.java
+++ app/src/main/java/org/acm/nitsurat/digitalcanteen/MainActivityFragment.java
@@ -1,15 +1,25 @@
 package org.acm.nitsurat.digitalcanteen;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     public MainActivityFragment() {
     }
@@ -17,6 +27,55 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return rootView;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getFragmentManager());
+        adapter.addFragment(new SouthIndianF(), "South Indian");
+        adapter.addFragment(new ChineseF(), "Chinese");
+        adapter.addFragment(new EggF(), "Egg Items");
+        adapter.addFragment(new NorthIndianF(),"North Indian");
+        viewPager.setAdapter(adapter);
+    }
+
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
